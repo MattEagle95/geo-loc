eb2cc736155c4028f47f653b256b90f5c91ce5b2
skrambl <55457634+MattEagle95@users.noreply.github.com>
2023-05-31 00:31:21 +0200

    9

---
+++ src/index.test.ts
@@ -0,0 +1,29 @@
+import {isOnLand, isOnWater} from "./index";
+
+const MOCK_COORDINATES = [{
+    x: 0,
+    y: 0,
+},{
+    x: 1,
+    y: 1,
+}];
+
+describe('isOnLand', () => {
+    test.each(MOCK_COORDINATES)('returns true for coordinates x: $x y: $y', ({x, y}) => {
+        expect(isOnLand(x, y)).toBeTruthy();
+    });
+
+    test.each(MOCK_COORDINATES)('returns false for coordinates x: $x y: $y', ({x, y}) => {
+        expect(isOnLand(x, y)).toBeFalsy();
+    });
+});
+
+describe('isOnWater', () => {
+    test.each(MOCK_COORDINATES)('returns true for coordinates x: $x y: $y', ({x, y}) => {
+        expect(isOnWater(x, y)).toBeTruthy();
+    });
+
+    test.each(MOCK_COORDINATES)('returns false for coordinates x: $x y: $y', ({x, y}) => {
+        expect(isOnWater(x, y)).toBeFalsy();
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Returns if the coordinates are on land
+ *
+ * @param x - asd
+ * @param y - asd
+ * @returns A boolean if the coordinate is on land or not.
+ */
+export const isOnLand = (x: number, y: number) => {
+    return true;
+}
+
+/**
+ * Returns if the coordinates are on land
+ *
+ * @param x - asd
+ * @param y - asd
+ * @returns A boolean if the coordinate is on land or not.
+ */
+export const isOnWater = (x: number, y: number) => {
+    return true;
+}
